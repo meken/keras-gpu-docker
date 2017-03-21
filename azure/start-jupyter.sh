1f8ee7ea5fd9edd16317091116b9d4b9e3296eb3
Murat Eken <eken.murat@outlook.com>
2017-03-21 08:38:59 +0100

    fixing the docker & keras versions as newer versions are not backward compatible, cleaning up the Azure template to utilize managed disks and some further improvements...

--- azure/start-jupyter.sh
+++ azure/start-jupyter.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
 NVIDIA_DRIVER_VERSION=375.26
+NVIDIA_DOCKER_VERSION=1.0.0
+DOCKER_VERSION=1.12.6-0~ubuntu-xenial
 
 # Getting ready for the NVIDIA driver installation
-sudo apt-get install -y build-essential
+apt-get update && apt-get install -y build-essential
 
 # Download & install the NVIDIA driver
 wget -P /tmp http://us.download.nvidia.com/XFree86/Linux-x86_64/$NVIDIA_DRIVER_VERSION/NVIDIA-Linux-x86_64-$NVIDIA_DRIVER_VERSION.run
 chmod u+x /tmp/NVIDIA-Linux*.run
-sudo /tmp/NVIDIA-Linux*.run --silent
+/tmp/NVIDIA-Linux*.run --silent
+
+
+# Install a specific version of docker
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list
+apt-get update && apt-get install -y docker-engine="$DOCKER_VERSION"
+
 
 # Assuming that docker is already installed, install nvidia-docker and nvidia-docker-plugin
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0/nvidia-docker_1.0.0-1_amd64.deb
-sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v$NVIDIA_DOCKER_VERSION/nvidia-docker_$NVIDIA_DOCKER_VERSION-1_amd64.deb
+dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
 
 # Docker needs absolute paths for volume mapping, retrieving the working directory
 BASE_DIR=`pwd`
