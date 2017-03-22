eee51f51acc66757c1e0238d1ae762db6f964aa7
Murat Eken <murat.eken@microsoft.com>
2017-03-22 16:58:48 +0100

    Clone & checkout the right branch...

--- azure/start-jupyter.sh
+++ azure/start-jupyter.sh
@@ -28,7 +28,7 @@ dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
 BASE_DIR=`pwd`
 
 # Checkout the code
-git clone https://github.com/meken/keras-gpu-docker.git keras
+git clone -b v2.0 https://github.com/meken/keras-gpu-docker.git keras
 
 cd keras/docker
 
